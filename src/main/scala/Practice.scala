bb6a949567b09cab851bbedd332f31c840a36f0e
Zwaks <zwakelemgabhi@gmail.com>
2024-07-01 13:11:29 +0200

    section 3: variables and strings

---
+++ src/main/scala/Practice.scala
@@ -0,0 +1,11 @@
+@main
+def funcName(): Unit = {
+  val catName = "My cat's name is \"Fluffy\""
+  println(catName)
+  
+  val slashes = "Two types of slashes \\ and /"
+  println(slashes)
+
+  val google = "http://www.google.com"
+  println(google)
+}
